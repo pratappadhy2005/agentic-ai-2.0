c28ff018b4a09a3fa4ddf829cfc5a4a8059fb54b
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-23 17:17:50 +1100

    -Added badic pydantic model with complex structures

--- 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/2-basic-pyadantic.py
+++ 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/2-basic-pyadantic.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from typing import List, Dict
 
 class Patient(BaseModel):
     name: str
     age: int
     weight: float
+    married: bool = False
+    allergies: List[str] = []
+    contact_details: Dict[str, str] = {}
+
 
 def insert_patient_date(patient: Patient):
     print(patient.name)
@@ -14,8 +19,12 @@ def insert_patient_date(patient: Patient):
 def update_patient_date(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
     print('Updated patient data')
 
-patient = Patient(name='Pratap', age=30, weight=70.5)
+patient = Patient(name='Pratap', age=30, weight=70.5, married=True, allergies=['Peanuts'], contact_details={'email': 'pratap@example.com', 'phone': '1234567890'})
 insert_patient_date(patient)
 update_patient_date(patient)
