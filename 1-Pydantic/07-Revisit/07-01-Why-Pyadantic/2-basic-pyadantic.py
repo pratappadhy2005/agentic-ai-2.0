eb175f617340777d8800bec21e5f084b2edba056
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-23 17:31:36 +1100

    -Added field validation with metadata

--- 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/2-basic-pyadantic.py
+++ 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/2-basic-pyadantic.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
-    name: str
-    email: EmailStr
+    name: str = Field(max_length=50, description='Name of the patient')
+    email: EmailStr = Field(..., description='Email of the patient')
     linkedInUrl: Optional[AnyUrl] = None
-    age: int
-    weight: float
-    married: bool = False
-    allergies: Optional[List[str]] = None
-    contact_details: Dict[str, str] = {}
+    age: int = Field(gt=0, lt=90, description='Age of the patient')
+    weight: float = Field(gt=0, lt=120, description='Weight in kilograms')
+    married: bool = Field(default=False, description='Married status')
+    allergies: Optional[List[str]] = Field(None, max_length=5, description='List of allergies')
+    contact_details: Dict[str, str] = Field({}, description='Contact details')
 
 
 def insert_patient_date(patient: Patient):
@@ -17,6 +17,7 @@ def insert_patient_date(patient: Patient):
     print(patient.email)
     print(patient.linkedInUrl)
     print(patient.age)
+    print(patient.weight)
     print('Inserted patient data')
 
 
@@ -31,6 +32,6 @@ def update_patient_date(patient: Patient):
     print(patient.contact_details)
     print('Updated patient data')
 
-patient = Patient(name='Pratap', email='pratap@example.com', linkedInUrl='https://www.linkedin.com/in/pratap-padhy/', age=30, weight=70.5,  contact_details={'email': 'pratap@example.com', 'phone': '1234567890'})
+patient = Patient(name='Pratap', email='pratap@example.com', linkedInUrl='https://www.linkedin.com/in/pratap-padhy/', age=30, weight=1,  contact_details={'email': 'pratap@example.com', 'phone': '1234567890'})
 insert_patient_date(patient)
 update_patient_date(patient)
