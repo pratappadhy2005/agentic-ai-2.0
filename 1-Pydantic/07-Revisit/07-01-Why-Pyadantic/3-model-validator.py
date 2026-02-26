307bf493bd85fb0d590ff4bc41befc82facecb68
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-26 21:40:42 +1100

    -Added compute filed example

--- 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/3-model-validator.py
+++ 1-Pydantic/07-Revisit/07-01-Why-Pyadantic/3-model-validator.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, EmailStr, model_validator
+from pydantic import BaseModel, EmailStr, model_validator, computed_field
 from typing import Annotated, List, Dict, Optional
 
 class Patient(BaseModel):
@@ -15,6 +15,15 @@ class Patient(BaseModel):
         if self.age > 60 and self.weight < 60:
             raise ValueError('Weight must be at least 60 kg for age above 60')
         return self 
+    
+    @computed_field
+    def is_adult(self) -> bool:
+        return self.age >= 18
 
-patient = Patient(name='Pratap', email='pratap@example.com', age=65, weight=50, married=False)
-print(patient)
+    @computed_field
+    def bmi(self) -> float:
+        return self.weight / (self.age ** 2)
+
+patient = Patient(name='Pratap', email='pratap@example.com', age=65, weight=70, married=False)
+print(patient.is_adult)
+print(patient.bmi)
